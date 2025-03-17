c2699a3220bb90fff50b8101a80b38f6c6df47e3
Enrique Lopez <95664476+ingenriquelopez@users.noreply.github.com>
2025-03-17 06:58:20 +0000

    app.py

--- src/app.py
+++ src/app.py
@@ -5,10 +5,9 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
 from api.models import db
-from api.routes import init_app
+from api.routes import init_app, api_bp  # Importar api_bp aquí
 from api.commands import setup_commands
 from flask_cors import CORS
-
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 
@@ -19,12 +18,9 @@ static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 print(os.getenv('FLASK_APP'))
-# database condiguration
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
-        "postgres://", "postgresql://")
 
+# Database configuration
+db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
         "postgres://", "postgresql://")
@@ -36,30 +32,28 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 
-# add the admin
+# Add the admin
 setup_commands(app)
 
-
+# Inicializar los blueprints con init_app
 init_app(app)
-# Add all endpoints form the API with a "api" prefix
-app.register_blueprint(api_bp, url_prefix='/api')
 
+# Registra el blueprint api_bp
+app.register_blueprint(api_bp, url_prefix='/api')  # Esto ya no da error porque ahora api_bp está importado correctamente
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-
-
-# generate sitemap with all your endpoints
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
+# Serve static files
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
@@ -68,7 +62,7 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
-# this only runs if `$ python src/main.py` is executed
+# Runs only if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
